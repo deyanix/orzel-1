3a9a1557cfc92efe910709a155f446764103f16e
deyanix <deyanix@deyanix.eu>
2024-11-14 02:25:22 +0100

    Improved IR handling, added USART

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stm32l476xx.h>
 #include <stm32l4xx_hal.h>
 #include <stm32l4xx_hal_tim.h>
+#include <memory.h>
+#include <stdio.h>
 
 TIM_HandleTypeDef htim2;
+UART_HandleTypeDef huart2;
 
 // 3 -> PB3
 #define M1_SPEED_PIN GPIO_PIN_3
@@ -17,22 +20,68 @@ TIM_HandleTypeDef htim2;
 #define M2_DIRECTION_PIN GPIO_PIN_6
 #define M2_DIRECTION_PORT GPIOA
 
-#define N_VAL   64
-volatile uint64_t ir_value = 0;
-size_t counter = 0;
+#define N_VAL   11
 
+typedef enum {
+    PULSE_SHORT,
+    PULSE_HIGH,
+    PULSE_UNKNOWN,
+} pulse_t;
+
+uint16_t lastValue;
+uint16_t currentValue;
+size_t counter;
+
+int _write(int file, char *ptr, int len) {
+    HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+    return len;
+}
+
+int __io_putchar(int ch) {
+    HAL_UART_Transmit(&huart2, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
+    return 1;
+}
+
+void Error_Handler(void) {
+    __disable_irq();
+    while (1);
+}
+
+pulse_t Decode_Pulse(uint32_t pulseWidth) {
+    if (pulseWidth < 300)
+        return PULSE_UNKNOWN;
+    if (pulseWidth < 900)
+        return PULSE_SHORT;
+    if (pulseWidth < 1800)
+        return PULSE_HIGH;
+
+    return PULSE_UNKNOWN;
+}
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-    uint32_t new_val;
+    uint32_t pulseWidth;
+    pulse_t pulse;
 
     if (htim == &htim2) {
         switch (HAL_TIM_GetActiveChannel(&htim2)) {
             case HAL_TIM_ACTIVE_CHANNEL_1:
-                new_val = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-                if (counter < N_VAL) {
-                    ir_value |= (new_val < 600 ? 1 : 0) << (counter++);
+                pulseWidth = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+                pulse = Decode_Pulse(pulseWidth);
+                if (pulse != PULSE_UNKNOWN) {
+                    if (pulse == PULSE_HIGH) {
+                        currentValue |= 1 << counter;
+                    }
+
+                    if (++counter == N_VAL) {
+                        if (lastValue != currentValue) {
+                            lastValue = currentValue;
+                        }
+                        counter = 0;
+                        currentValue = 0;
+                    }
                 } else {
                     counter = 0;
+                    currentValue = 0;
                 }
                 break;
             default:
@@ -64,17 +113,73 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base) {
     }
 }
 
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    if(huart->Instance==USART2)
+    {
+        /* USER CODE BEGIN USART2_MspInit 0 */
+
+        /* USER CODE END USART2_MspInit 0 */
+
+        /** Initializes the peripherals clock
+        */
+        PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+        PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+        if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+        {
+            Error_Handler();
+        }
+
+        /* Peripheral clock enable */
+        __HAL_RCC_USART2_CLK_ENABLE();
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**USART2 GPIO Configuration
+        PA2     ------> USART2_TX
+        PA3     ------> USART2_RX
+        */
+        GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN USART2_MspInit 1 */
+
+        /* USER CODE END USART2_MspInit 1 */
+
+    }
+
+}
+
+/**
+* @brief UART MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+    if(huart->Instance==USART2) {
+        __HAL_RCC_USART2_CLK_DISABLE();
+
+        HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
+    }
+}
+
 void SysTick_Handler(void) {
     HAL_IncTick();
 }
 
 void TIM2_IRQHandler(void) {
     HAL_TIM_IRQHandler(&htim2);
-}
-
-void Error_Handler(void) {
-    __disable_irq();
-    while (1);
 }
 
 void SystemClock_Config(void) {
@@ -105,6 +210,22 @@ void SystemClock_Config(void) {
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+void MX_USART2_UART_Init(void) {
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    if (HAL_UART_Init(&huart2) != HAL_OK) {
         Error_Handler();
     }
 }
@@ -154,7 +275,7 @@ void MX_TIM2_Init(void) {
     htim2.Instance = TIM2;
     htim2.Init.Prescaler = 79;
     htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim2.Init.Period = 4294967295;
+    htim2.Init.Period = 0xFFFFFFFF;
     htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
     if (HAL_TIM_Base_Init(&htim2) != HAL_OK) {
@@ -179,7 +300,7 @@ void MX_TIM2_Init(void) {
     if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) {
         Error_Handler();
     }
-    sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_BOTHEDGE;
+    sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
     sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
     sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
     sConfigIC.ICFilter = 0;
@@ -192,19 +313,16 @@ int main(void) {
     HAL_Init();
 
     SystemClock_Config();
+    MX_USART2_UART_Init();
     MX_GPIO_Init();
     MX_TIM2_Init();
 
     HAL_TIM_Base_Start(&htim2);
     HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
 
-    HAL_GPIO_TogglePin(M1_SPEED_PORT, M1_SPEED_PIN);
-    HAL_GPIO_TogglePin(M2_SPEED_PORT, M2_SPEED_PIN);
-    HAL_GPIO_TogglePin(M1_DIRECTION_PORT, M1_DIRECTION_PIN);
-
     while (1) {
-        HAL_GPIO_TogglePin(M1_DIRECTION_PORT, M1_DIRECTION_PIN);
-        HAL_GPIO_TogglePin(M2_DIRECTION_PORT, M2_DIRECTION_PIN);
-        HAL_Delay(1000);
+        HAL_Delay(500);
+        printf("Value: 0x%04X\r\n", lastValue);
+        lastValue = 0;
     }
 }
